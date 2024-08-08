3b55d3dfaeaf6e8dcc12d57f3cd1517448a8f1cd
Ryan Parsley <rmparsley@gmail.com>
2024-08-08 02:43:06 +0000

    Updated 2 files via CloudCannon.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import remarkMermaid from "remark-mermaidjs";
 export default defineConfig({
 	site: "https://ryanparsley.com",
 	markdown: {
-		remarkPlugins: [[remarkMermaid, {mermaidConfig: { theme: 'dark' }}]],
+		//remarkPlugins: [[remarkMermaid, {mermaidConfig: { theme: 'dark' }}]],
 	},
 	integrations: [
 		mdx(),
