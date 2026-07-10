4600da3b7ab67405c962aa1d0876efe7dddb08b2
Ryan Parsley <RMParsley@gmail.com>
2026-07-09 23:19:22 -0400

    chore: fix eslint config, add .mjs to format script, npm audit fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,29 +2,11 @@ import eslintPluginAstro from "eslint-plugin-astro";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-import parser from "astro-eslint-parser";
-
 export default [
 	{
 		ignores: ["src/env.d.ts"],
 	},
 	eslint.configs.recommended,
-	...eslintPluginAstro.configs.recommended,
 	...tseslint.configs.recommended,
-	{
-		files: ["**/*.astro"],
-
-		languageOptions: {
-			parser: parser,
-			ecmaVersion: "latest",
-			sourceType: "module",
-
-			parserOptions: {
-				parser: "@typescript-eslint/parser",
-				extraFileExtensions: [".astro"],
-			},
-		},
-
-		rules: {},
-	},
+	...eslintPluginAstro.configs.recommended,
 ];
